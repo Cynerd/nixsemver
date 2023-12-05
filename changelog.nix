76092a768ad1b098047a5282c0eac1b6be3b57ff
Karel Kočí <cynerd@email.cz>
2023-12-05 18:02:51 +0100

    changelog: add currentRelease

--- changelog.nix
+++ changelog.nix
@@ -1,13 +1,14 @@
-_: super: let
+self: super: let
   inherit (builtins) split elemAt filter isList;
-  inherit (super) fileContents head flatten;
+  inherit (super) fileContents head flatten optionalString;
+  inherit (self) semverSplit semverToString;
 in rec {
   /*
   Returns list of releases that are recorded in the changelog fiel provided.
 
   Example:
     releases ./CHANGELOG.md
-    => ["Unreleasead" "1.0.0" "0.1.0"]
+    => ["Unreleased" "1.0.0" "0.1.0"]
   */
   releases = p: flatten (filter isList (split "\n## \\[([^]]*)]" (fileContents p)));
 
@@ -16,9 +17,9 @@ in rec {
   the one right after it. The default is 0.0.0 if there is no release yet.
 
   Example:
-    latest_release ["Unreleased" "1.0.0" "0.1.0" ]
+    latestRelease ["Unreleased" "1.0.0" "0.1.0" ]
     => "1.0.0"
-    latest_release ["1.0.0" "1.0.1" "0.1.0"]
+    latestRelease ["1.0.0" "1.0.1" "0.1.0"]
     => "1.0.0"
   */
   latestRelease = list: let
@@ -32,8 +33,33 @@ in rec {
   Combination of releases and latest_release.
 
   Example:
-    get_latest_release ./CHANGELOG.md
+    getLatestRelease ./CHANGELOG.md
     => "1.0.0"
   */
   getLatestRelease = p: latestRelease (releases p);
+
+  /*
+  This combines the flake's source info with changelog version to produce the
+  appropriate version. The primary idea is to add short git revision to the
+  pre-release field if changelog has Unreleased section.
+
+  Example:
+    currentRelease ./CHANGELOG.md self.sourceInfo
+    => "1.1.0-alpha0.ae849da-dirty+docker"
+  */
+  currentRelease = p: sourceInfo: let
+    r = releases p;
+    lr = latestRelease r;
+    slr = semverSplit lr;
+  in
+    if head r == "Unreleased"
+    then
+      semverToString (slr
+        // {
+          preRelease =
+            (optionalString (slr.preRelease
+                != null) "${slr.preRelease}.")
+            + "${sourceInfo.shortRev or sourceInfo.dirtyShortRev}";
+        })
+    else lr;
 }
